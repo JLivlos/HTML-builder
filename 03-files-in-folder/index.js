989e3e47c80cabca26af174c1842b1e787d35590
JLivlos <mistlivlos@gmail.com>
2022-11-05 01:56:48 +0300

    fix: 03. get info about files with unusual names

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,23 +3,21 @@ const fsPromises = require('fs/promises');
 const fs = require('fs');
 const folderPath = path.join(__dirname, 'secret-folder');
 
-async function letData() {
+async function getData() {
     try {
         const data = await fsPromises.readdir(folderPath, { withFileTypes: true });
 
-        let files = [];
         data.forEach(function(file) {
-            if (file.isFile() === true) files.push(file.name);
-        });
-
-        for (let i = 0; i < files.length; i++) {
-            fs.stat(path.join(folderPath, files[i]), (err, stat) => {
-                let arr = files[i].split('.');
-                console.log(`${arr[0]} - ${arr[1]} - ${+stat.size / 1000}kb`);
+            if (file.isFile() === true) {
+                let name = `${file.name.split(`${path.extname(file.name)}`).join('')}`;
+                let ext = `${path.extname(file.name).slice(1, (file.name).length - 1)}`;
+                fs.stat(path.join(folderPath, file.name), (err, stat) => {
+                console.log(`${name} - ${ext} - ${+stat.size / 1000}kb`);          
             })
-        }
+        };                
+        });        
     } catch (err) {
         console.error(err);
     };
 };
-letData();
+getData();
